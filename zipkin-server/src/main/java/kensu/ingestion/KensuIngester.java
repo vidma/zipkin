2f1cd0c02f659f79532e8c1df02c41723b927021
vidmantas zemleris <vidma@kensu.io>
2021-10-11 11:05:19 +0300

    wip2

--- zipkin-server/src/main/java/kensu/ingestion/KensuIngester.java
+++ zipkin-server/src/main/java/kensu/ingestion/KensuIngester.java
@@ -1,5 +1,20 @@
 package kensu.ingestion;
 
-public class KensuIngester {
+import io.kensu.collector.config.KensuTracerFactory;
+import zipkin2.Span;
 
+import java.util.List;
+
+public class KensuIngester {
+  void ingestTraces(List<List<Span>> traces) {
+    KensuTracerFactory.prepareTracer(); // FIXME!
+
+    for (List<Span> trace: traces){
+      ingestTrace(trace);
+    }
+  }
+
+  void ingestTrace(List<Span> traces) {
+
+  }
 }
